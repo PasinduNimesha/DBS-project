f5d9d5b2dfdc43b12523a5a4b68af19388acfb05
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2024-08-25 14:16:50 +0530

    Update DbConfig.java

--- backend/hrm_java/hrm/src/main/java/com/Jupiter/hrm/config/DbConfig.java
+++ backend/hrm_java/hrm/src/main/java/com/Jupiter/hrm/config/DbConfig.java
@@ -1,15 +1,29 @@
 package com.Jupiter.hrm.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Component
 public class DbConfig {
-    public static Connection getConnection() throws SQLException {
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    public Connection getConnection() throws SQLException {
         Connection con = null;
 
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jupiter", "root", "30104771");
+            con = DriverManager.getConnection(url, username, password);
             System.out.println("Connection success");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -19,5 +33,3 @@ public class DbConfig {
         return con;
     }
 }
-
-
